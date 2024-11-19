bcf91c1684457aeb6fd4d431ceb6cc5a16d7ab22
Alexander Dietrich <alexander@dietrich.cx>
2024-11-19 21:28:05 +0100

    Add older Firefox version support

--- background.js
+++ background.js
@@ -2,6 +2,8 @@
 
 const MENU_ITEM_ID = 'copy-link-and-text';
 
+var legacyBrowser = false;
+
 function escapeHTML (html) {
     return html
       .replaceAll('&', '&amp;')
@@ -20,29 +22,34 @@ function markdownLink(url, text) {
 }
 
 function copyToClipboard (links) {
-  let htmlValue = "";
-  let textValue = "";
+  let htmlValue = '';
+  let textValue = '';
 
   if (links.length === 1) {
     htmlValue = htmlLink(links[0].url, links[0].text);
     textValue = markdownLink(links[0].url, links[0].text);
   } else {
-    htmlValue += "<ul>\n";
+    htmlValue += '<ul>\n';
     for (let link of links) {
       htmlValue += `<li>${htmlLink(link.url, link.text)}</li>\n`;
       textValue += `- ${markdownLink(link.url, link.text)}\n`;
     }
-    htmlValue += "</ul>\n";
+    htmlValue += '</ul>\n';
   }
 
-  let item = new ClipboardItem({'text/html': htmlValue, 'text/plain': textValue});
-  navigator.clipboard.write([item])
-    .catch(error => console.error(`Copy link failed: ${error.message}`));
-}
-
-function onMessage (message) {
-  if (message.type === 'clat.link_response') {
-    copyToClipboard(message.links);
+  if (!legacyBrowser) {
+    let item = new ClipboardItem({'text/html': htmlValue, 'text/plain': textValue});
+    navigator.clipboard.write([item])
+      .catch(error => console.error(`Copy link failed: ${error.message}`));
+  } else {
+    let legacyListener = function (event) {
+      event.clipboardData.setData('text/html', htmlValue);
+      event.clipboardData.setData('text/plain', textValue);
+      event.preventDefault();
+    }
+    document.addEventListener('copy', legacyListener);
+    document.execCommand('copy');
+    document.removeEventListener('copy', legacyListener);
   }
 }
 
@@ -69,4 +76,12 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
   }
 });
 
-browser.runtime.onMessage.addListener(message => onMessage(message));
+browser.runtime.onMessage.addListener(message => {
+  if (message.type === 'clat.link_response') {
+    copyToClipboard(message.links);
+  }
+});
+
+if (typeof ClipboardItem === 'undefined') {
+  legacyBrowser = true;
+}
