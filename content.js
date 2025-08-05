b8e3588fe0ecec6da90df591919d910f3078055b
Alexander Dietrich <alexander@dietrich.cx>
2025-08-05 23:54:33 +0200

    Create text fragments from selected ranges

--- content.js
+++ content.js
@@ -13,6 +13,7 @@ function onMessage (message) {
 
   let links = [];
 
+  // Try to gather URLs from selected A elements
   for (let i = 0; i < selection.rangeCount; i++) {
     let range = selection.getRangeAt(i);
     let fragment = range.cloneContents();
@@ -22,11 +23,43 @@ function onMessage (message) {
     }
   }
 
-  if (links.length == 0) {
-    links.push({'url': document.URL, 'text': document.title})
+  // Try to create text fragments from selected ranges
+  if (links.length === 0 && selection.toString().trim() !== '') {
+    let fragments = [];
+
+    for (let i = 0; i < selection.rangeCount; i++) {
+      let fragment = '';
+
+      let range = selection.getRangeAt(i);
+      let text = range.toString().trim();
+      let words = text.split(' ');
+      if (words.length > 10) {
+        let start = words.slice(0, 5).join(' ');
+        let end = words.slice(-5).join(' ');
+        fragment = `${encodeFragment(start)},${encodeFragment(end)}`;
+      } else {
+        fragment = encodeFragment(text);
+      }
+
+      fragments.push(`text=${fragment}`);
+    }
+
+    let url = new URL(document.URL);
+    url.hash = '';
+    url = `${url.toString()}#:~:${fragments.join('&')}`;
+    links.push({'url': url, 'text': document.title});
+  }
+
+  // Otherwise use the page URL
+  if (links.length === 0) {
+      links.push({'url': document.URL, 'text': document.title});
   }
 
   browser.runtime.sendMessage({'type': LINK_RESPONSE, 'links': links});
+}
+
+function encodeFragment(text) {
+  return encodeURIComponent(text).replaceAll('-', '%2D');
 }
 
 browser.runtime.onMessage.addListener(message => onMessage(message));
